671f852cb1efaefcf99a035bb8f25f14ef6d7368
table-en4 <mohapouge@gmail.com>
2025-07-01 17:29:36 +0200

    hidenp easier

---
+++ Level 3/hidenp.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	if (ac == 3)
+	{
+		while (av[1][i])
+		{
+			while (av[2][j])
+			{
+				if (av[1][i] == av[2][j])
+				{
+					k++;
+					break;
+				}
+				j++;
+			}
+			i++;
+		}
+		if (k == i)
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
+	}
+	write(1, "\n", 1);
+}
