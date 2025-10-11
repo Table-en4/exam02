78d4261c9b26d155bd7f6e488944fc3923276be6
Mohamed Lapouge <molapoug@f1r13s2.clusters.42paris.fr>
2025-10-11 14:36:33 +0200

    modif de itoa

--- Level 4/ft_itoa.c
+++ Level 4/ft_itoa.c
@@ -1,48 +1,29 @@
 #include <stdlib.h>
 
-int	count(int n)
+int	ft_count_len(int nbr)
 {
 	int	i = 0;
-	//compter la taille de notre
-	//nombre
-	if (n <= 0)
+	if (nbr <= 0)
 		i++;
-	while (n != 0)
-	{
-		n /= 10;
-		i++;
-	}
+	while (nbr != 0)
+		1 &&( nbr /= 10, i++);
 	return i;
 }
 
-char	*ft_iota(int n)
+char	*ft_itoa(int nbr)
 {
 	int	i;
-	// déclarer la taille du nombre
-	int	j = count(n);
-	long	num = n;
-	//malloc la taille du nomnbre
-	char *str = (char *)malloc((sizeof(char) * (j + 1)));
+	int	len = ft_count_len(nbr);
+	long	num = nbr;
+	char	*str = (char *)malloc((sizeof(char) * (len + 1)));
 	if (!str)
 		return NULL;
-	//donner le '\0' a la fin de la string
-	str[j] = '\0';
-	//ne pas prendre en compte le  '\0'
-	//pour la suite des calculs
-	i = j - 1;
-	//gerer les negatifs
+	str[len] = '\0';
+	i = len -1;
 	if (num < 0)
-	{
-		str[0] = '-';
-		num = -num;
-	}
+		1 && (str[0] = '-', num = -num);
 	while (num > 9)
-	{
-		//faire la converion de int a char
-		str[i] = num % 10 + '0';
-		num /= 10;
-		i--;
-	}
-	str[i] = num + '0';
-	return (str);
+		1 && (str[i] = num % 10 + '0', num /= 10, i--);
+	return (str[i] = num + '0', str);
 }
+
